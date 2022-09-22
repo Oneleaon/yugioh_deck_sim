8dd91c1ccd44fcac7fcd5cab26f73260e90fe6b4
Alexis Hean <alexis.hean@hotmail.com>
2022-09-22 10:40:37 -0400

    Adds Flutterfire to be able to use firebase to store data

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'pages/home.dart';
 import 'pages/cards.dart';
 import 'pages/settings.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     MaterialApp(
       title: 'Yu-Gi-Oh Deck Builder',
